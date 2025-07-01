de9929302427586d92b32d3ec4f947b97f3964f1
nagisaShiyoto <elon.gilad9@gmail.com>
2025-07-01 15:35:18 +0300

    biniding tests

--- managment/managment/managment.cpp
+++ managment/managment/managment.cpp
@@ -1,6 +1,14 @@
-#include "Remote_management.h"
+#define WIN32_LEAN_AND_MEAN
+
 #include <iostream>
 #include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "Remote_management.h"
+#define DEFULT_PORT "1234"
+// Need to link with Ws2_32.lib
+#pragma comment(lib, "ws2_32.lib")
+
 
 int main() {
     HANDLE mutex = CreateMutexA(NULL, FALSE, "my_mutex");
@@ -10,4 +18,23 @@ int main() {
     }
     RemoteManagement manager;
     return 0;
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int err;
+
+    /* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+    wVersionRequested = MAKEWORD(2, 2);
+
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if (err != 0) {
+        /* Tell the user that we could not find a usable */
+        /* Winsock DLL.                                  */
+        printf("WSAStartup failed with error: %d\n", err);
+        return 1;
+    }
+    addrinfo* addr;
+    getaddrinfo(NULL, DEFULT_PORT, NULL, &addr);
+    SOCKET listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int result = bind(listeningSocket,addr->ai_addr, addr->ai_addrlen);
+    std::cout << "asdasd" << std::endl;
 }
