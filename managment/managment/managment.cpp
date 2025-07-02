ac0ec08c797f7755a3355658933b67e3b554ae89
nagisaShiyoto <elon.gilad9@gmail.com>
2025-07-02 13:18:48 +0300

    fix: change var name and change it to get general errors

--- managment/managment/managment.cpp
+++ managment/managment/managment.cpp
@@ -7,13 +7,7 @@ int main() {
 
     try {
         RemoteManagement manager;
-    } catch (const MutexException e) {
-        std::cout << "couldn't create mutex:" << std::endl;
-        std::cout << e.getError() << std::endl;
-    } catch (const ShortPathException e) {
-        std::cout << e.getError() << std::endl;
-    } catch (const RegistryException e) {
-        std::cout << "registry error:" << std::endl;
+    } catch (const GeneralExceptionClass e) {
         std::cout << e.getError() << std::endl;
     }
     return 0;
